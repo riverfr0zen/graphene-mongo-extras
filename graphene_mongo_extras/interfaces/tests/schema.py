494bb58e5b4d3efa6d26d0c49e385e54da60e574
Irfan Baig <riverfr0zen@elitemail.org>
2019-07-08 09:06:19 -0400

    WIP: as above

---
+++ graphene_mongo_extras/interfaces/tests/schema.py
@@ -0,0 +1,50 @@
+import graphene
+from graphene_mongo import MongoengineObjectType, MongoengineConnectionField
+from graphene_mongo_extras import MongoengineInterface
+from graphene.relay import Node, Connection
+from graphene_mongo_extras.interfaces.tests.models import Toy, Packaging, \
+                                                          Plushie, Videogame
+
+
+class ToyInterface(MongoengineInterface):
+    class Meta:
+        model = Toy
+
+
+class PackagingType(MongoengineObjectType):
+    class Meta:
+        model = Packaging
+        interfaces = (Node,)
+        connection_class = Connection
+        connection_field_class = MongoengineConnectionField
+
+
+class PlushieType(MongoengineObjectType):
+    class Meta:
+        model = Plushie
+        interfaces = (Node, ToyInterface,)
+        # interfaces = (ToyInterface,)
+        connection_class = Connection
+        connection_field_class = MongoengineConnectionField
+
+
+class VideogameType(MongoengineObjectType):
+    class Meta:
+        model = Videogame
+        interfaces = (Node, ToyInterface,)
+        # interfaces = (ToyInterface,)
+        connection_class = Connection
+        connection_field_class = MongoengineConnectionField
+
+
+class Query(graphene.ObjectType):
+    toys = graphene.List(ToyInterface)
+
+    def resolve_toys(self, info):
+        return list(Toy.objects.all())
+
+
+schema = graphene.Schema(
+    query=Query,
+    types=[PackagingType, PlushieType, VideogameType]
+)
--- graphene_mongo_extras/interfaces/tests/test_mongoengine_interface.py
+++ graphene_mongo_extras/interfaces/tests/test_mongoengine_interface.py
@@ -1,11 +1,8 @@
 import pytest
-import graphene
 from graphene.test import Client
-from graphene_mongo import MongoengineObjectType, MongoengineConnectionField
-from graphene_mongo_extras import MongoengineInterface
-from graphene.relay import Node, Connection
 from graphene_mongo_extras.interfaces.tests.models import Toy, Packaging, \
                                                           Plushie, Videogame
+from graphene_mongo_extras.interfaces.tests.schema import schema
 
 
 @pytest.fixture
@@ -29,50 +26,6 @@ def setup_data():
     dspak = Packaging(name="DSPak").save()
     Videogame(name="Dark Souls", genre="masochism",
               packaging=dspak).save()
-
-
-class ToyInterface(MongoengineInterface):
-    class Meta:
-        model = Toy
-
-
-class PackagingType(MongoengineObjectType):
-    class Meta:
-        model = Packaging
-        interfaces = (Node,)
-        connection_class = Connection
-        connection_field_class = MongoengineConnectionField
-
-
-class PlushieType(MongoengineObjectType):
-    class Meta:
-        model = Plushie
-        interfaces = (Node, ToyInterface,)
-        # interfaces = (ToyInterface,)
-        connection_class = Connection
-        connection_field_class = MongoengineConnectionField
-
-
-class VideogameType(MongoengineObjectType):
-    class Meta:
-        model = Videogame
-        interfaces = (Node, ToyInterface,)
-        # interfaces = (ToyInterface,)
-        connection_class = Connection
-        connection_field_class = MongoengineConnectionField
-
-
-class Query(graphene.ObjectType):
-    toys = graphene.List(ToyInterface)
-
-    def resolve_toys(self, info):
-        return list(Toy.objects.all())
-
-
-schema = graphene.Schema(
-    query=Query,
-    types=[PackagingType, PlushieType, VideogameType]
-)
 
 
 def test_mongoengine_interface(setup_data, cleanup):
