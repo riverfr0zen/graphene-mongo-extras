6390179ced7cc6640a489a41949a2b83eaa44c81
Irfan Baig <riverfr0zen@elitemail.org>
2019-07-11 08:59:16 -0400

    WIP: test global id to mongo id

--- graphene_mongo_extras/interfaces/tests/test_mongoengine_interface.py
+++ graphene_mongo_extras/interfaces/tests/test_mongoengine_interface.py
@@ -1,4 +1,6 @@
 import pytest
+from bson.objectid import ObjectId
+from graphql_relay import from_global_id, to_global_id
 from graphene.test import Client
 from graphene_mongo_extras.interfaces.tests.models import Toy, Packaging, \
                                                           Plushie, Videogame
@@ -6,33 +8,40 @@ from graphene_mongo_extras.interfaces.tests.schema import schema
 
 
 @pytest.fixture
-def cleanup():
-    yield
+def setup_data():
+    fifipak = Packaging(name="FifiPak").save()
+    fifi = Plushie(name="fifi", animal="dog",
+                   packaging=fifipak).save()
+    kittypak = Packaging(name="KittyPak").save()
+    kitty = Plushie(name="kitty", animal="cat",
+                    packaging=kittypak).save()
+    contrapak = Packaging(name="ContraPak").save()
+    contra = Videogame(name="Contra", genre="shmup",
+                       packaging=contrapak).save()
+    dspak = Packaging(name="DSPak").save()
+    ds = Videogame(name="Dark Souls", genre="masochism",
+                   packaging=dspak).save()
+    yield {
+        'fifipak': fifipak,
+        'fifi': fifi,
+        'kittypak': kittypak,
+        'kitty': kitty,
+        'contrapak': contrapak,
+        'contra': contra,
+        'dspak': dspak,
+        'ds': ds
+    }
+    # Cleanup
     Toy.drop_collection()
     Packaging.drop_collection()
 
 
-@pytest.fixture
-def setup_data():
-    fifipak = Packaging(name="FifiPak").save()
-    Plushie(name="fifi", animal="dog",
-            packaging=fifipak).save()
-    kittypak = Packaging(name="KittyPak").save()
-    Plushie(name="kitty", animal="cat",
-            packaging=kittypak).save()
-    contrapak = Packaging(name="ContraPak").save()
-    Videogame(name="Contra", genre="shmup",
-              packaging=contrapak).save()
-    dspak = Packaging(name="DSPak").save()
-    Videogame(name="Dark Souls", genre="masochism",
-              packaging=dspak).save()
-
-
-def test_mongoengine_interface(setup_data, cleanup):
+def test_mongoengine_interface(setup_data):
 
     client = Client(schema)
     res = client.execute('''{
         toys {
+            id
             name
             packaging {
                 name
@@ -48,6 +57,10 @@ def test_mongoengine_interface(setup_data, cleanup):
     assert 'data' in res
     assert len(res['data']['toys']) == 4
     assert 'packaging' in res['data']['toys'][0]
+    d_fifi = res['data']['toys'][0]
+    d_fifi_type, d_fifi_id = from_global_id(d_fifi['id'])
+    assert d_fifi_type == 'PlushieType'
+    assert d_fifi_id == str(setup_data['fifi'].pk)
     assert res['data']['toys'][0]['packaging']['name'] == 'FifiPak'
     assert 'animal' in res['data']['toys'][0]
     assert res['data']['toys'][0]['animal'] == 'dog'
